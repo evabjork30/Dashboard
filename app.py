6d7681d1427ed8bc73204fd1908261e02bf482b2
Eva Björk Davíðsdóttir <evad24@ru.is>
2025-02-28 05:39:13 +0000

    same for major type table

--- app.py
+++ app.py
@@ -343,16 +343,16 @@ with col3:
     for col in numeric_columns:
         formatted_table[col] = formatted_table[col].astype(float).map("{:.2f}".format)
 
-    # Remove empty rows by resetting the index
-    cleaned_table = formatted_table.dropna(how="all") #.reset_index()
-
     # Transpose the table for better readability
-    table_data_transposed = cleaned_table.T  # Makes majors columns instead of rows
+    table_data_transposed = formatted_table.T  # Makes majors columns instead of rows
+
+    # Remove empty rows by resetting the index
+    cleaned_table = table_data_transposed.dropna(how="all").reset_index()
 
     # Display the table with better formatting
     st.write("#### 📋 Summary Statistics by Major Type")
     st.dataframe(
-        table_data_transposed,  # Ensures 2 decimal places
+        cleaned_table,  # Ensures 2 decimal places
         height=300, width=900  # Adjusts the table size to avoid excessive scrolling
     )
 
